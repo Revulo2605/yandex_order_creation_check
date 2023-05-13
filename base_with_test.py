19b86f42d0bca6e3ef0eda57fe43e5c91b4a0d33
Revulo <asv2605@yandex.ru>
2023-05-13 12:54:27 +0300

    Fix code

--- base_with_test.py
+++ base_with_test.py
@@ -2,21 +2,24 @@ import data
 import sender_requests
 
 
-# Функция на создание заказа и сохранения её трек-номера
-def get_track_number(body):
+# Функция создание заказа и формирование params для запроса на получение этого заказа
+def get_params_of_track_number(body):
     # 1. Отправим запрос на создание заказа и запишем тело ответа
     order_response = sender_requests.post_new_order(body)
-    # 2. Перепишем заголовок header_track_number в data.py
-    data.param_track_number["t"] = order_response.json()["track"]
-    # Всё заказ создан, а его трек-номер сохранён
+    # 2. Подготовим основу для запроса к получению заказа по трек-номеру
+    params_tack_number = data.param_track_number["t"]
+    # 3. Запишем актуальный трек-номер заказа в params_tack_number
+    params_tack_number["t"] = order_response.json()["track"]
+    # Всё заказ создан, а его трек-номер сформирован и сохранён. Выведем её на выход этой функции
+    return params_tack_number
 
 
 # Тесты:
 # Проверим, что после запроса на создание заказа, он был внесен в таблицу Orders
 def test_check_order_by_his_number():
-    # 1. Отправим запрос на создание заказа и сохраним его трек-номер в data.param_track_number
-    get_track_number(data.body_order_correct)
+    # 1. Отправим запрос на создание заказа и сохраним его трек-номер в виде params
+    params_for_get_order = get_params_of_track_number(data.body_order_correct)
     # 2. Отправим запрос на получение заказа по трек-номеру, что мы получили выше, и запишем ответ
-    body_request = sender_requests.get_order_by_his_number(data.param_track_number)
+    body_request = sender_requests.get_order_by_his_number(params_for_get_order)
     # 3. Проверим, что бэкенд нашел заказ с таким трек-номером в таблице Orders
     assert body_request.status_code == 200
